7cf3e72a08342fc4a17032c411d1e55488aa11a5
Krisztián Pometkó <pometko.k@gmail.com>
2020-06-09 14:30:07 +0200

    Added api controller

--- GameDex_backend/Models/User.cs
+++ GameDex_backend/Models/User.cs
@@ -7,10 +7,11 @@ namespace GameDex_backend.Models
 {
     public class User
     {
+        public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public DateTime RegisterDate { get; set; }
-        public int[] FavGames { get; set; }
+        //public List<int> FavGames { get; set; }
         public string Auth_token { get; set; }
     }
 }
