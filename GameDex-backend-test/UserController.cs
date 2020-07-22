e9820c0e50097a6ef655864c9a4002f6193aa5b5
Krisztián <Krisztian@pometko.hu>
2020-07-22 13:37:17 +0200

    Added test class for UserController

---
+++ GameDex-backend-test/UserController.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace GameDex_backend_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
